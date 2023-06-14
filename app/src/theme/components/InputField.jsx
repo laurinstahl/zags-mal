17bfe9621b8f8289294c14eb0130c22e0030a3cc
Laurin Lukas Stahl <laurinstahl@googlemail.com>
2023-06-15 00:09:18 +0200

    created processing state for microphone recording

--- app/src/theme/components/InputField.jsx
+++ app/src/theme/components/InputField.jsx
@@ -8,7 +8,24 @@ import { Box } from '@chakra-ui/react';
  */
 export const InputField = ({ onSend, onRecord, ...props }) => {
   const [message, setMessage] = React.useState('');
+  const [processing, setProcessing] = React.useState(false);
+  const [recording, setRecording] = React.useState(false);
+  const [mediaRecorder, setMediaRecorder] = React.useState(null);
+  const [loadingText, setLoadingText] = React.useState('');
   const textareaRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (processing) {
+      let i = 0;
+      const intervalId = setInterval(() => {
+        setLoadingText('.'.repeat(i++ % 4));
+      }, 500);
+      return () => clearInterval(intervalId);
+    } else {
+      setLoadingText('');
+    }
+  }, [processing]);
+
 
   const handleSend = () => {
     if(message.trim() !== ''){
@@ -24,6 +41,26 @@ export const InputField = ({ onSend, onRecord, ...props }) => {
     }
   };
 
+  const handleRecord = async () => {
+    if (!recording) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+        setMediaRecorder(mediaRecorder);
+        setRecording(true);
+      } catch (err) {
+        console.error('Failed to start recording', err);
+      }
+    } else {
+      mediaRecorder.stop();
+      setRecording(false);
+      // Simulate processing delay
+      setProcessing(true);
+      setTimeout(() => setProcessing(false), 3000);  // Remove this when real processing is implemented
+    }
+  };
+
   React.useEffect(() => {
     textareaRef.current.style.height = "24px"; // Reset the height
     textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
@@ -33,15 +70,16 @@ export const InputField = ({ onSend, onRecord, ...props }) => {
     <Box p="16px">
       <div className="input-field">
         <textarea
+          value={processing ? loadingText : message}
           ref={textareaRef}
-          value={message}
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={handleKeyDown}
           className="input-text"
+          disabled={processing}
           {...props}
         />
         <button onClick={handleSend} className="send-button" disabled={message.trim() === ''}>Send</button>
-        <button onClick={onRecord} className="record-button">🎙️</button>
+        <button onClick={handleRecord} className="record-button">{recording ? '⏹️' : '🎙️'}</button>
       </div>
     </Box>
   );
