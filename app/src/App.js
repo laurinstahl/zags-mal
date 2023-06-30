7d23eccb569188d2c0f7a64838c2e5dfb2eef5ee
Laurin Lukas Stahl <laurinstahl@googlemail.com>
2023-06-30 15:44:36 +0200

    build

--- app/src/App.js
+++ app/src/App.js
@@ -13,7 +13,7 @@ if (process.env.REACT_APP_ENV === 'PROD') {
   growthbookKey = process.env.REACT_APP_GROWTHBOOK_PROD
 }
 console.log(growthbookKey)
-// Create a GrowthBook instance
+// Create a GrowthBook instanc
 const gb = new GrowthBook({
   apiHost: "https://cdn.growthbook.io",
   clientKey: growthbookKey,
